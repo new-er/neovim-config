2436cced906396bda58871c89a8ba2894b6e3f8b
Neuer <dominik.schraml97@gmail.com>
2024-08-22 01:21:43 +0200

    add undotree

--- init.lua
+++ init.lua
@@ -115,6 +115,8 @@ vim.keymap.set('n', '<leader>h9', function()
   require('harpoon.ui').nav_file(9)
 end, { desc = '[h]arpoon file [9]' })
 
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+vim.g.undotree_DiffCommand = 'FC'
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -186,6 +188,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'ThePrimeagen/harpoon',
+  'mbbill/undotree',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
