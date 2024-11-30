505051b95899149adebcdec206e6ee7b390b3495
new-er <new-er@new-er.com>
2024-11-30 20:40:44 +0100

    fix keymap deletion error

--- lua/options_nvim_tree.lua
+++ lua/options_nvim_tree.lua
@@ -4,9 +4,9 @@ local function nvimTreeOnAttach(bufnr)
   api.config.mappings.default_on_attach(bufnr)
 
   -- delete keymaps which collide with vim-tmux-navigator
-  vim.keymap.del('n', '<C-h>', { buffer = bufnr })
-  vim.keymap.del('n', '<C-j>', { buffer = bufnr })
+  --vim.keymap.del('n', '<C-h>', { buffer = bufnr })
+  --vim.keymap.del('n', '<C-j>', { buffer = bufnr })
   vim.keymap.del('n', '<C-k>', { buffer = bufnr })
-  vim.keymap.del('n', '<C-l>', { buffer = bufnr })
+  --vim.keymap.del('n', '<C-l>', { buffer = bufnr })
 end
 require('nvim-tree').setup({ view = { relativenumber = true, number = true, }, on_attach = nvimTreeOnAttach })
